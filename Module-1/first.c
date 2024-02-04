d604c425476dc9af12e6959acf92b0485ac540b7
Muyaz khan <muyazkhan06@gmail.com>
2024-02-04 19:35:30 +0600

    Basic C Program

--- Module-1/first.c
+++ Module-1/first.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 int main()
 {
-  printf("hello lol");
+  printf("hello yuu");
   return 0;
 }
-
- 
---
+++ Module-1_Basic syntex/varriable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int rahim=10;
+    float karim=10000.52;
+    char habib='Z';
+    printf("%d\n%0.1f\n%c",rahim,karim,habib);
+    return 0;
+}
